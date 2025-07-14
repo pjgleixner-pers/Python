d0406b58c8fb88bf78f62de954fb8dcaba903426
Pieter Gleixner <pjgleixner@msn.com>
2025-07-14 15:24:43 -0400

    Create ForLoop.py

---
+++ ForLoop.py
@@ -0,0 +1,9 @@
+halo_peeps = ["Master chief","Cortana","captain keys","Flood"]
+
+for i in halo_peeps:
+    print(f"{i} is a character from Halo")
+    
+    #or
+    
+for i in range(len(halo_peeps)):
+    print(f"{halo_peeps[i]} is a character from Halo")
